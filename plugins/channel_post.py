cf6efefb9705d2f4f1b8545ff3137c25e05cb1a1
jonysins2211 <Jonysins11220@gmail.com>
2025-11-10 00:37:20 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -8,7 +8,8 @@ from helper_func import encode
 
 
 # ---- Admin uploads or forwarded files ---
-@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
+#@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
+@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start', 'users', 'broadcast', 'batch', 'genlink', 'stats', 'movie', 'series', 'createpost', 'attach', 'preview']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Please Wait...!", quote=True)
     try:
