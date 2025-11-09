2524ee681db95be305603234be57af07ed8e9f77
jonysins2211 <Jonysins11220@gmail.com>
2025-11-10 00:06:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,31 @@
 from aiohttp import web
 from plugins import web_server
 import pyromod.listen
-from pyrogram import Client
+from pyrogram import Client, filters
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
-from config import API_HASH, API_ID, LOGGER, BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, CHANNEL_ID, PORT
+from config import (
+    API_HASH,
+    API_ID,
+    LOGGER,
+    BOT_TOKEN,
+    TG_BOT_WORKERS,
+    FORCE_SUB_CHANNEL,
+    FORCE_SUB_CHANNEL2,
+    CHANNEL_ID,
+    PORT,
+)
 import pyrogram.utils
 import asyncio
 
-pyrogram.utils.MIN_CHANNEL_ID = -1009999999999
+# 🧩 Fix: Ignore slash commands globally in text filters
+filters.not_command = filters.create(
+    lambda _, __, m: not (m.text and m.text.startswith("/"))
+)
 
+# 🧩 Compatibility fix for large channel IDs
+pyrogram.utils.MIN_CHANNEL_ID = -1009999999999
 
 
 class Bot(Client):
@@ -21,7 +36,7 @@ class Bot(Client):
             api_id=API_ID,
             plugins={"root": "plugins"},
             workers=TG_BOT_WORKERS,
-            bot_token=BOT_TOKEN
+            bot_token=BOT_TOKEN,
         )
         self.LOGGER = LOGGER
 
@@ -30,6 +45,7 @@ class Bot(Client):
         usr_bot_me = await self.get_me()
         self.uptime = datetime.now()
 
+        # Force subscribe channel 1
         if FORCE_SUB_CHANNEL:
             try:
                 link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
@@ -39,10 +55,18 @@ class Bot(Client):
                 self.invitelink = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot Can't Export Invite Link From Force Sub Channel !")
-                self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNEL Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support For Support")
+                self.LOGGER(__name__).warning(
+                    "Bot Can't Export Invite Link From Force Sub Channel!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Please Double Check The FORCE_SUB_CHANNEL Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission. Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}"
+                )
+                self.LOGGER(__name__).info(
+                    "\nBot Stopped. https://t.me/MadflixBots_Support For Support"
+                )
                 sys.exit()
+
+        # Force subscribe channel 2
         if FORCE_SUB_CHANNEL2:
             try:
                 link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
@@ -52,27 +76,41 @@ class Bot(Client):
                 self.invitelink2 = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot Can't Export Invite Link From Force Sub Channel !")
-                self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNEL2 Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support For Support")
+                self.LOGGER(__name__).warning(
+                    "Bot Can't Export Invite Link From Force Sub Channel 2!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Please Double Check The FORCE_SUB_CHANNEL2 Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission. Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}"
+                )
+                self.LOGGER(__name__).info(
+                    "\nBot Stopped. https://t.me/MadflixBots_Support For Support"
+                )
                 sys.exit()
 
+        # Database channel check
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Hey 🖐")
+            test = await self.send_message(chat_id=db_channel.id, text="Hey 🖐")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure Bot Is Admin In DB Channel, And Double Check The CHANNEL_ID Value, Current Value: {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/MadflixBots_Support For Support")
+            self.LOGGER(__name__).warning(
+                f"Make Sure Bot Is Admin In DB Channel, And Double Check The CHANNEL_ID Value, Current Value: {CHANNEL_ID}"
+            )
+            self.LOGGER(__name__).info(
+                "\nBot Stopped. Join https://t.me/MadflixBots_Support For Support"
+            )
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated By \nhttps://t.me/Madflix_Bots")
-        self.LOGGER(__name__).info(f"""ミ💖 MADFLIX BOTZ 💖彡""")
+        self.LOGGER(__name__).info(
+            f"Bot Running..!\n\nCreated By \nhttps://t.me/Madflix_Bots"
+        )
+        self.LOGGER(__name__).info("ミ💖 MADFLIX BOTZ 💖彡")
         self.username = usr_bot_me.username
-        #web-response
+
+        # Web response setup
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
@@ -82,25 +120,15 @@ class Bot(Client):
         await super().stop()
         self.LOGGER(__name__).info("Bot Stopped...")
 
+
 def run(self):
-        """Run the bot."""
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
-        try:
-            loop.run_forever()
-        except KeyboardInterrupt:
-            self.LOGGER(__name__).info("Shutting down...")
-        finally:
-            loop.run_until_complete(self.stop())
-            
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+    """Run the bot."""
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(self.start())
+    self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        self.LOGGER(__name__).info("Shutting down...")
+    finally:
+        loop.run_until_complete(self.stop())
